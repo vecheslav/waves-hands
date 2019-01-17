c9a17af1538405bc8983ddc7e89670d59762124b
Vecheslav Druzhbin <vecheslavdruzbhin@gmail.com>
2019-01-17 19:24:47 +0300

    fix bug join

--- src/app/matches/shared/matches.helper.ts
+++ src/app/matches/shared/matches.helper.ts
@@ -61,6 +61,9 @@ const compareMoves = (m1: number, m2: number) =>
     (m1 === 2 && m2 === 1)) ? 1 : (m1 === m2 ? 0 : -1)
 
 export const whoHasWon = (p1: number[], p2: number[]) => {
+  if (!p1 || !p2) {
+    return
+  }
   const score = p2.slice(0, 3).reduce((s, p2move, i) => s + compareMoves(p1[i], p2move), 0)
   return score > 0 ? MatchResult.Creator : (score === 0 ? MatchResult.Draw : MatchResult.Opponent)
 }
