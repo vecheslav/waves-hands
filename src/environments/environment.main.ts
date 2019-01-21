e6b699b9665fe0b9a8952f7fa416f49f6c36cd59
Vecheslav Druzhbin <vecheslavdruzbhin@gmail.com>
2019-01-21 12:25:27 +0300

    env fixes

--- src/environments/environment.main.ts
+++ src/environments/environment.main.ts
@@ -3,11 +3,11 @@ import { ChainId } from './chain'
 export const environment = {
   production: true,
   gameBetAmount: 100000000,
-  chainId: ChainId.Testnet,
+  chainId: ChainId.Mainnet,
   defaultTimeout: 1000 * 60,
   api: {
-    baseEndpoint: 'https://testnodes.wavesnodes.com/',
-    txEnpoint: 'https://api.testnet.wavesplatform.com/v0/'
+    baseEndpoint: 'https://nodes.wavesnodes.com/',
+    txEnpoint: 'https://api.wavesplatform.com/v0/'
   },
   broadcastRetryLimit: 10,
   retryDelay: 1000,
