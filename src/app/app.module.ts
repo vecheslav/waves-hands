fc56e0b242fed6d1c12e1781729df5229f892c04
Vecheslav Druzhbin <vecheslavdruzbhin@gmail.com>
2019-01-22 11:15:53 +0300

    added error capture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule, Injectable, ErrorHandler } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -9,7 +9,22 @@ import { AuthModule } from './auth/auth.module'
 import { CoreModule } from './core/core.module'
 import { MatchesModule } from './matches/matches.module'
 import { UserModule } from './user/user.module'
-import { ActionsModule } from './actions/actions.module';
+import { ActionsModule } from './actions/actions.module'
+
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({
+  dsn: 'https://d6f8eab0064d4728b57bc20654e111ff@sentry.io/1376566'
+})
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    Sentry.captureException(error.originalError || error)
+    throw error
+  }
+}
 
 @NgModule({
   declarations: [
@@ -26,7 +41,7 @@ import { ActionsModule } from './actions/actions.module';
     UserModule,
     ActionsModule,
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
