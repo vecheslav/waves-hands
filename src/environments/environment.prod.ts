16ca39f2c28cf199d1477cdae6a03de7d2ad5d2c
Vecheslav Druzhbin <vecheslavdruzbhin@gmail.com>
2019-01-30 16:19:34 +0300

    to mainnet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,15 +4,15 @@ export const environment = {
   production: true,
   gameBetAmount: 100000000,
   defaultFee: 100000,
-  chainId: ChainId.Testnet,
+  chainId: ChainId.Mainnet,
   defaultTimeout: 1000 * 60,
   api: {
-    baseEndpoint: 'https://testnodes.wavesnodes.com/',
-    txEnpoint: 'https://api.testnet.wavesplatform.com/v0/',
-    timeStart: '2019-01-29T00:00:00.388Z'
+    baseEndpoint: 'https://nodes.wavesnodes.com/',
+    txEnpoint: 'https://api.wavesplatform.com/v0/',
+    timeStart: '2019-01-29T00:00:00.000Z'
   },
   broadcastRetryLimit: 10,
   retryDelay: 1000,
-  creatorRevealBlocksCount: 4,
-  serviceAddress: '3N7oF5J5m9BwBFCmVJZWr3AEH3u4c6CLoB9'
+  creatorRevealBlocksCount: 15,
+  serviceAddress: '3PFdhb7QmEGHLh8YGHKspAKeeVCEhqxdUfM'
 }
