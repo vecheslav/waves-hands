d74d92938bac2df0dc1190d9d88ca98d5afbd51d
Yuriy Naydenov <ebceu44@gmail.com>
2019-02-05 17:00:14 +0300

    contract test

--- src/app/matches/shared/contract.ts
+++ src/app/matches/shared/contract.ts
@@ -2,78 +2,96 @@ import { environment } from 'src/environments/environment'
 import { compile } from '@waves/ride-js'
 
 export const scriptCode = `
-
 let me = tx.sender
 let serviceAddress = addressFromString("${environment.serviceAddress}")
 let heightKey = "height"
+let player2Key = "player2Key"
+let player1KeyKey = "player1Key"
+let p1MoveHashKey = "p1MoveHash"
+let matchKeyKey = "matchKey"
+let paymentKey = "payment"
 let stage1 = "p2MoveHash"
 let stage2 = "p2Move"
 let stage3 = "p1Move"
 let wave = 100000000
-let gameBet = 1*wave
-let serviceCommission = 200
+let gameBet = 1 * wave
+let serviceCommission = gameBet / 200
 let rock = base58'1'
 let paper = base58'2'
 let scissors = base58'3'
-let matchReservationBlocksCount = 3
-let creatorRevealBlocksCount = ${environment.creatorRevealBlocksCount}
-let maxlong = 9223372036854775807
+let player2ReservationTimeout = 3
+let player1RevealTimeout =  ${environment.creatorRevealBlocksCount}
 
-let matchKey = extract(getBinary(me, "matchKey"))
-let player1Key = extract(getBinary(me, "player1Key"))
+#Extracting all required keys from predefined state (state before contract applied)
+let p1MoveHash = extract(getBinary(me, p1MoveHashKey))
+let matchKey = extract(getBinary(me, matchKeyKey))
+let player1Key = extract(getBinary(me, player1KeyKey))
 
 match (tx) {
-    case dataTx:DataTransaction =>   
-        let dataTxFeeIsOk = dataTx.fee == 500000
-        let dataTxDataIsOk =
-        if isDefined(getBinary(dataTx.data, stage1)) then
+    case dataTx:DataTransaction => 
 
-        let oldHeight = if(isDefined(getInteger(me, heightKey))) then extract(getInteger(me, heightKey)) else maxlong
-        let canWriteP2Move = (!isDefined(getBinary(me, stage1)) || (oldHeight - height < -matchReservationBlocksCount && !isDefined(getBinary(me, stage2))))
-        let isDataSizeValid = size(dataTx.data) == 3        
-        let isHeightFieldCorrect = 
-        (height == extract(getInteger(dataTx.data, heightKey)) ||
-        height - 1 == extract(getInteger(dataTx.data, heightKey)) ||
-        height + 1 == extract(getInteger(dataTx.data, heightKey)))
+    let feeValid = dataTx.fee == 500000
+  
+    let payloadValid = 
+    #Player2 reserving match for himself
+    if isDefined(getBinary(dataTx.data, stage1)) then 
 
-        canWriteP2Move && isDataSizeValid && isHeightFieldCorrect
+    let h = extract(getInteger(dataTx.data, heightKey))
 
-        else if isDefined(getBinary(dataTx.data, stage2)) then 
-            let isSizeOk = size(dataTx.data) == 2
-            let isNotDefined = !isDefined(getBinary(me, stage2))
-            let isHashShaOk = sha256(extract(getBinary(dataTx.data, stage2))) == extract(getBinary(me, stage1))
-            let isPaymentOk = match (transactionById(extract(getBinary(dataTx.data, "payment")))) {
-                case p2payment:TransferTransaction =>
-                    let isAttSizeOk = size(p2payment.attachment) == 32 
-                    let isAttHashOk = sha256(p2payment.attachment) == extract(getBinary(me, stage1)) 
-                    isAttSizeOk &&
-                    isAttHashOk &&
-                    p2payment.amount == 1*wave &&
-                    !isDefined(p2payment.assetId) &&
-                    p2payment.recipient == me &&
-                    p2payment.senderPublicKey == extract(getBinary(me, "player2Key")) &&
-                    sigVerify(p2payment.bodyBytes, p2payment.proofs[0], p2payment.senderPublicKey)
-                case _ => false
-              }
-            isNotDefined &&
-            isSizeOk &&
-            isHashShaOk &&
-            isPaymentOk
+    let hasValidReservation = if(isDefined(getInteger(me, heightKey))) then height - extract(getInteger(me, heightKey)) <= player2ReservationTimeout else false
+    let hasPlayer2MoveRevealed = isDefined(getBinary(me, stage2))
+    let heightValid = h <= height + 1 && h >= height - 1
+    let isPlayer2KeyDefined = isDefined(getBinary(dataTx.data, player2Key))
+    let sizeValid = size(dataTx.data) == 3
+    
+    !hasValidReservation && !hasPlayer2MoveRevealed && heightValid && isPlayer2KeyDefined && sizeValid
+    #Player2 revealing his moves and linking to his payment
+    else if isDefined(getBinary(dataTx.data, stage2)) then
 
-        else if isDefined(getBinary(dataTx.data, stage3)) then
-            let isP1NotYetRevealed = !isDefined(getBinary(me, stage3))
-            let isSizeValid = size(dataTx.data) == 1
-            let isProofValid = size(extract(getBinary(dataTx.data, stage3))) == 32
-            let isShaValid = sha256(extract(getBinary(dataTx.data, stage3))) == extract(getBinary(me, "p1MoveHash"))
+    let p2MoveHash = extract(getBinary(me, stage1))
 
-            isP1NotYetRevealed && isSizeValid && isProofValid && isShaValid
-        else false      
+    let player2RevealedMoves = isDefined(getBinary(me, stage2))
+    let sizeValid = size(dataTx.data) == 2
+    let hashValid = sha256(extract(getBinary(dataTx.data, stage2))) == p2MoveHash
+    let paymentValid = match (transactionById(extract(getBinary(dataTx.data, paymentKey)))) {
+        case p2payment:TransferTransaction =>
+            sha256(p2payment.attachment) == p2MoveHash &&
+            p2payment.amount == 1*wave &&
+            p2payment.recipient == me &&
+            p2payment.senderPublicKey == extract(getBinary(me, player2Key)) &&
+            !isDefined(p2payment.assetId) &&
+            sigVerify(p2payment.bodyBytes, p2payment.proofs[0], p2payment.senderPublicKey)
+        case _ => false
+      }
+    
+    !player2RevealedMoves && hashValid && sizeValid && paymentValid
+    #Player1 revealing his moves
+    else if isDefined(getBinary(dataTx.data, stage3)) then
+    
+    let player1RevealedMoves = isDefined(getBinary(me, stage3))
+    let sizeValid = size(dataTx.data) == 1
+    let proofValid = size(extract(getBinary(dataTx.data, stage3))) == 32
+    let hashValid = sha256(extract(getBinary(dataTx.data, stage3))) == extract(getBinary(me, p1MoveHashKey))
 
-        dataTxFeeIsOk &&
-        dataTxDataIsOk
+    !player1RevealedMoves && sizeValid && proofValid && hashValid
+    
+    else false
 
+    feeValid && payloadValid
+  
     case payout:MassTransferTransaction => 
-    let waitingTooLongForMatchCreatorToReveal = extract(getInteger(me, heightKey)) - height < -creatorRevealBlocksCount
+
+    let isCommissionIncluded =
+      payout.transfers[0].recipient == serviceAddress &&
+      payout.transfers[0].amount == serviceCommission
+
+    let feeValid =
+      (payout.transferCount == 2 && payout.fee <= 600000) ||
+      (payout.transferCount == 3 && payout.fee <= 700000)
+
+    let player1RevealedMoves = isDefined(getBinary(me, stage3))    
+    let waitingTooLongForPlayer1 = height - extract(getInteger(me, heightKey)) > player1RevealTimeout && !player1RevealedMoves
+
     let p1moves = take(extract(getBinary(me, stage3)), 3)
     let p2moves = take(extract(getBinary(me, stage2)), 3)
     let p1m1 = take(p1moves, 1)
@@ -82,7 +100,7 @@ match (tx) {
     let p2m2 = takeRight(take(p2moves, 2), 1)
     let p1m3 = takeRight(p1moves, 1)
     let p2m3 = takeRight(p2moves, 1)
-    
+
     let round1 = if (p1m1 == rock && p2m1 == scissors) || (p1m1 == paper && p2m1 == rock) || (p1m1 == scissors && p2m1 == paper) then +1 else if p1m1 == p2m1 then 0 else -1
     let round2 = if (p1m2 == rock && p2m2 == scissors) || (p1m2 == paper && p2m2 == rock) || (p1m2 == scissors && p2m2 == paper) then +1 else if p1m2 == p2m2 then 0 else -1
     let round3 = if (p1m3 == rock && p2m3 == scissors) || (p1m3 == paper && p2m3 == rock) || (p1m3 == scissors && p2m3 == paper) then +1 else if p1m3 == p2m3 then 0 else -1
@@ -90,28 +108,23 @@ match (tx) {
     let score = round1 + round2 + round3
 
     let p1 = addressFromPublicKey(player1Key)
-    let p2 = addressFromPublicKey(extract(getBinary(me, "player2Key")))
+    let p2 = addressFromPublicKey(extract(getBinary(me, player2Key)))
 
-    let isCommissionIncluded =
-        payout.transfers[0].recipient == serviceAddress &&
-        payout.transfers[0].amount == gameBet / serviceCommission
-    
-    let isFeeIsNotAbused = (payout.transferCount == 2 && payout.fee <= 600000) || (payout.transferCount == 3 && payout.fee <= 700000)
+    let payoutValid =
+      (waitingTooLongForPlayer1 && payout.transfers[1].recipient == p2) ||   
+      if(score == 0) then
+          size(payout.transfers) == 3 &&
+          payout.transfers[1].amount == payout.transfers[2].amount &&
+          payout.transfers[1].recipient == p1 &&
+          payout.transfers[2].recipient == p2
+      else 
+          size(payout.transfers) == 2 &&
+          payout.transfers[1].recipient == if score > 0 then p1 else p2
 
-    let isPayoutValid = (waitingTooLongForMatchCreatorToReveal && payout.transfers[1].recipient == p2) ||   
-    if(score == 0) then
-        size(payout.transfers) == 3 &&
-        payout.transfers[1].amount == payout.transfers[2].amount &&
-        payout.transfers[1].recipient == p1 &&
-        payout.transfers[2].recipient == p2
-    else 
-        let winner = if score > 0 then p1 else p2
-        size(payout.transfers) == 2 &&
-        payout.transfers[1].recipient == winner
+    feeValid && isCommissionIncluded && payoutValid
     
-    isFeeIsNotAbused && isCommissionIncluded && isPayoutValid
-    case _ => false  
-}
+    case _ => false
+  }
 
 `
 export const compiledScript = Buffer.from(compile(scriptCode).result).toString('base64')
