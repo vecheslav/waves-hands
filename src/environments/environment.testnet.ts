caf32c0d0990044ce9b25e81059e02792b263af7
Vecheslav Druzhbin <vecheslavdruzhbin@gmail.com>
2019-02-06 13:57:01 +0300

    added testnet config

---
+++ src/environments/environment.testnet.ts
@@ -0,0 +1,18 @@
+import { ChainId } from './chain'
+
+export const environment = {
+  production: true,
+  gameBetAmount: 100000000,
+  defaultFee: 100000,
+  chainId: ChainId.Testnet,
+  defaultTimeout: 1000 * 60,
+  api: {
+    baseEndpoint: 'https://testnodes.wavesnodes.com/',
+    txEnpoint: 'https://api.testnet.wavesplatform.com/v0/',
+    timeStart: '2019-01-29T00:00:00.388Z'
+  },
+  broadcastRetryLimit: 10,
+  retryDelay: 1000,
+  creatorRevealBlocksCount: 4,
+  serviceAddress: '3N7oF5J5m9BwBFCmVJZWr3AEH3u4c6CLoB9'
+}
